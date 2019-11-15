3134cc589f59ff23fec30c680d6ac6dd2a03de29
pavelexpertov <pavelm01@hotmail.co.uk>
2019-11-15 21:12:37 +0000

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-sudo cp etc/X11/xorg.conf.d/40-sennheiser-gsx-1000.conf /etc/X11/xorg.conf.d
-sudo cp lib/udev/rules.d/91-pulseaudio-gsx1000.rules /lib/udev/rules.d/
-sudo cp -r usr/share/pulseaudio/alsa-mixer/profile-sets/sennheiser-gsx-1000.conf /usr/share/pulseaudio/alsa-mixer/profile-sets/
+sudo cp etc/X11/xorg.conf.d/40-sennheiser-gsx-1200.conf /etc/X11/xorg.conf.d
+sudo cp lib/udev/rules.d/91-pulseaudio-gsx1200.rules /lib/udev/rules.d/
+sudo cp -r usr/share/pulseaudio/alsa-mixer/profile-sets/sennheiser-gsx-1200.conf /usr/share/pulseaudio/alsa-mixer/profile-sets/
 sudo udevadm control -R
 sudo udevadm trigger
 
